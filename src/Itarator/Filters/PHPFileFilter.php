25cef3e424550762342d99b7e15d54a15eda7d8c
Alexey Puchkov <alexey@oktopost.com>
2016-12-14 21:52:34 +0200

    Add AllFilter

---
+++ src/Itarator/Filters/PHPFileFilter.php
@@ -0,0 +1,18 @@
+<?php
+namespace Itarator\Filters;
+
+
+use Itarator\IFilter;
+
+
+class PhpFileFilter implements IFilter
+{
+	/**
+	 * @param string $path
+	 * @return bool True if this item should be consumer
+	 */
+	public function filter($path)
+	{
+		// TODO: Implement filter() method.
+	}
+}
---
+++ tests/Itarator/Filters/PHPFileFilterTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexey
+ * Date: 14/12/16
+ * Time: 21:51
+ */
+
+namespace Itarator\Filters;
+
+
+class PhpFileFilterTest
+{
+
+}
